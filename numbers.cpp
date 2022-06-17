b872f6bf3adff9b2cb1dcccfeab745320c900005
Devin Jhu <devin.jhu@mths.ca>
2022-06-17 01:10:38 +0000

    code

--- numbers.cpp
+++ numbers.cpp
@@ -1,38 +1,68 @@
 // Copyright (C) 2022 Devin Jhu All rights reserved
 //
 // Created by Devin Jhu
-// Created on May 2022
-// The loop adder
+// Created on June 2022
+// The string to list converter
 
 #include <iostream>
-#include <random>
+#include <iomanip>
+#include <string>
+#include <list>
+
+
+int digitList(std::list<int> &numberList, int userNumber) {
+    // this function returns the digits
+
+    // process
+    while (userNumber) {
+        numberList.push_front(userNumber % 10);
+        userNumber /= 10;
+    }
+}
+
 
 int main() {
-    // this function finds the average of 10 random numbers
+    // this function gets user input and returns output
+    std::cout << "This program takes a number and returns its digits."
+              << std::endl;
+    std::cout << "" << std::endl;
 
-    int numberArray[10];
-    int randomNumber;
-    float average;
+    // variables
+    std::string userInput;
+    int userNumber;
+    std::list<int> numberList;
 
-    // random seed
-    std::random_device rseed;
-    std::mt19937 rgen(rseed());  // mersenne twister
-    std::uniform_int_distribution<int> idist(0, 100);  // random from 0 - 100
+    // input
+    while (true) {
+        try {
+            std::cout << "Enter any number: ";
+            std::cin >> userInput;
+            userNumber = std::stoi(userInput);
+            std::cout << "" << std::endl;
 
-    std::cout << "The random number average finder" << std::endl;
+            if (userNumber > 0) {
+                // call function
+                digitList(numberList, userNumber);
 
-    // process and output
-    for (int counter = 0; counter < 10; counter++) {
-        randomNumber = idist(rgen);
-        numberArray[counter] = randomNumber;
-        std::cout << "Random number: " << randomNumber << std::endl;
+                // printing out each digit from the list
+                std::cout << "The digits that make up the number is [";
+
+                for (int digits : numberList) {
+                    std::cout << digits;
+                    std::cout << ", ";
+                }
+
+                std::cout << "]" << std::endl;
+
+                break;
+
+            } else {
+                std::cout << "not a number, try again." << std::endl;
+            }
+        } catch (std::invalid_argument) {
+            std::cout << "not a number" << std::endl;
+        }
     }
-
-    average = std::accumulate(std::begin(numberArray), std::end(numberArray),
-        0, std::plus<int>());
-    average /= 10;
-
-    std::cout << "\nThe average is " << average << "." << std::endl;
-
     std::cout << "\nDone." << std::endl;
 }
+
